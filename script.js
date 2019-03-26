c1af7026ae57f1719384fd13fdaeec44f9cb2fd0
die10jp <d.nagai2register@gmail.com>
2019-03-27 00:57:52 +0900

    add: typing.io color change extension

---
+++ script.js
@@ -0,0 +1,40 @@
+document.title = 'hoge';
+document.querySelector('.header-wrapper').remove();
+document.querySelector('.footer').remove();
+document.querySelector('body').style.backgroundColor='#073642';
+
+
+let mo = new MutationObserver(() => {
+  document.querySelector('.texture').style.backgroundColor='#073642';
+  document.querySelector('.texture').style.background='url()';
+  document.querySelector('.code').style.margin='10px';
+  document.querySelector('.code').style.width='100%';
+
+  let x = document.querySelectorAll('.code > span');
+  for(var i = 0; i < x.length; i++) {
+    if (x[i].className === 'c initial') {
+      x[i].style.setProperty("color", "#657b83", "important");
+    } else {
+      //x[i].style.color='#93a1a1';
+      x[i].style.color='#eee';
+    }
+  }
+  let repeating = document.querySelector('.repeating');
+  if (repeating) {
+    repeating.parentNode.removeChild(repeating);
+  }
+  let finished = document.querySelector('.finished-main');
+  if (finished) {
+    console.log(document.querySelectorAll('.breakdown-inner > tbody > tr > .value'));
+    let wpm = document.querySelectorAll('.breakdown-inner > tbody > tr > .value')[2].innerText;
+    let next = document.querySelector('.next');
+    finished.textContent = null;
+    alert(wpm);
+    if (next) {
+      location.href = next.href;
+    }
+  }
+});
+
+let main = document.querySelector('#main');
+mo.observe(main, { childList: true, subtree: true });
