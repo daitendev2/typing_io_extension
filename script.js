be271552bd16a33cdf9154027736f0aa2f99acf9
die10jp <d.nagai2register@gmail.com>
2019-05-24 12:41:56 +0900

    script

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-document.title = 'hoge';
+document.title = 'develop';
 
 let mo = new MutationObserver(() => {
-
-/*
-  let x = document.querySelectorAll('.code > span');
-  for(var i = 0; i < x.length; i++) {
-    if (x[i].className === 'c initial') {
-      x[i].style.setProperty("color", "#657b83", "important");
-    } else {
-      x[i].style.color='#eee';
-    }
-  }
-*/
   let finished = document.querySelector('.finished-main');
   if (finished) {
     let wpm = document.querySelectorAll('.breakdown-inner > tbody > tr > .value')[2].innerText;
     let next = document.querySelector('.next');
-    finished.textContent = null;
-    if confirm(wpm) {
-      location.href = next.href;
+    mo.disconnect();
+    score = 'wpm: ' + wpm;
+    finished.textContent = score;
+    if (next) {
+      if (confirm(score)) {
+        location.href = next.href;
+      }
     }
   }
 });
