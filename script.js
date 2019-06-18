5d1c28f5a8001cf157f53a09f1346ac5b1e50e46
die10jp <d.nagai2register@gmail.com>
2019-06-18 14:57:52 +0900

    fix: add style

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.title = 'develop';
+document.title = 'shiftcmdf';
 
 let mo = new MutationObserver(() => {
   let finished = document.querySelector('.finished-main');
   if (finished) {
     let wpm = document.querySelectorAll('.breakdown-inner > tbody > tr > .value')[2].innerText;
     let next = document.querySelector('.next');
-    mo.disconnect();
     score = 'wpm: ' + wpm;
     finished.innerHTML = score;
+    mo.disconnect();
     if (next) {
       if (confirm(score)) {
         location.href = next.href;
